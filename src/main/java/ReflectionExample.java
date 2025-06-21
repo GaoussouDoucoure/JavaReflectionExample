23dc818511d5151f6e57fe5b724d4d62e91137bb
Gaoussou Doucoure <gaousd4@gmail.com>
2025-06-20 20:14:35 -0700

    Implemented the code to do the Reflection

--- src/main/java/ReflectionExample.java
+++ src/main/java/ReflectionExample.java
@@ -1,2 +1,39 @@
+import java.lang.reflect.Method;
+
+class GreeterService extends Object{
+    public void sayHello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public void anotherMethod() {
+        System.out.println("This is another method.");
+    }
+}
+
 public class ReflectionExample {
+
+    public static void main(String[] args) {
+        try {
+            // 1. Get the Class object for a class
+            Class<?> c = GreeterService.class; // Or Class.forName("ReflectionExample.MyClass");
+
+            // 2. Get all public methods declared in the class
+            Method[] methods = c.getMethods();
+
+            System.out.println("Methods in MyClass:");
+            for (Method method : methods) {
+                System.out.println("\t - " + method.getName());
+            }
+
+            // 3. Invoke a specific method by name
+            Object instance = c.getDeclaredConstructor().newInstance(); // Create an instance of MyClass
+            Method specificMethod = c.getMethod("sayHello", String.class); // Get the 'sayHello' method
+
+            // Invoke the method on the instance with arguments
+            specificMethod.invoke(instance, "World");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
